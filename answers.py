79c85527aa21322ef32925869a07050d27debe1f
reemasharma22 <reemasharma22@hotmail.com>
2021-06-18 10:58:15 +0000

    message

---
+++ answers.py
@@ -0,0 +1,66 @@
+#question1
+
+def one(str):
+    result = ''
+    for char in str:
+result += char * 2
+
+return result
+
+#question2
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+
+    return True
+
+    
+
+#question3 
+
+def three(a):
+    a1 = three * 1
+    a2 = three * 2
+    a3 = three * 3
+    a4 = three * 4
+    
+    total = int(a1) + int(a2) + int(a3) + int(a4)
+    print(total)
+
+
+
+
+#question4
+
+def four(string1, string2):
+    string1 = (Dog)
+    string2 = (Cat)
+    
+    zipped = zip(string1, string2)
+    print(zipped)
+    
+
+
+#question5
+
+import random
+list = [random.randint(100,250)
+
+#question6
+
+def six(string):
+    string = string.lower()
+    if string 
+
+
+#question7
+
+if(a==b&&b!=c||a==c&&c!=b||b==c&&c!=a){
+      return false;
+    }
+  return a-b==b-c||b-c==c-a||c-a==a-b;
+} 
+
+
+
